4cc63cda6a9735dd2304be6e73d712325e28c9bb
panzhen1998 <panzhen2022@126.com>
2024-11-28 23:28:26 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-data = pd.read_csv('C:/Users/27854/Desktop/test.csv')
+data = pd.read_csv('C:\Users\27854\Desktop\test.csv')
 df = pd.DataFrame(data)
 
 
